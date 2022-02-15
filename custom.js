ace84122d20b4924aa56383242bdf722c874ca3f
Tomnoy1060 <abunaimtonmoy60@gmail.com>
2022-02-15 12:56:53 +0600

    finish

---
+++ custom.js
@@ -0,0 +1,21 @@
+// all heading color change
+let firstHeading = document.getElementById('first-heading');
+firstHeading.style.color = 'white';
+let secondHeading = document.getElementById('second-heading');
+secondHeading.style.color = 'white';
+let thirdHeading = document.getElementById('third-heading');
+thirdHeading.style.color = 'white';
+
+// top player bg color
+const containerBgColor = document.getElementById('container-bg-color');
+containerBgColor.style.backgroundColor = 'lightseagreen';
+const secondContainerBgColor = document.getElementById('second-container-bg-color');
+secondContainerBgColor.style.backgroundColor = 'lightseagreen';
+
+// course card design
+const firstCardBgColor = document.getElementById('first-course');
+firstCardBgColor.style.backgroundColor = 'lavender';
+const secondCardBgColor = document.getElementById('second-course');
+secondCardBgColor.style.backgroundColor = 'lavender';
+const thirdCardBgColor = document.getElementById('third-course');
+thirdCardBgColor.style.backgroundColor = 'lavender';
